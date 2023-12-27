a19596f0f039062011119b543aedf587ead801fc
crcj <cr.cj@outlook.com>
2023-12-27 11:28:41 -0300

    tests completed

--- tests/test_di.py
+++ tests/test_di.py
@@ -26,15 +26,39 @@ def test_convert_new_contract_code():
     assert result == contract_maturity
 
 
-def test_settlement_rate_with_old_holiday_list_1():
-    # Reference date is before the new holiday calendar
+def test_settlement_rate_with_old_holiday_list():
+    settlement_rates = {
+        "N27": 9.809,
+        "F33": 10.368,
+    }
+
+    # 22-12-2023 is before the new holiday calendar
     df = dip.get_di_data(reference_date="22-12-2023")
-    result = df.query("contract_code == 'F33'")["settlement_rate"].values[0]
-    assert result == 10.368
+    contract_codes = list(settlement_rates.keys())
+    result = df.query("contract_code in @contract_codes")["settlement_rate"].to_list()
+    assert result == list(settlement_rates.values())
 
 
-def test_settlement_rate_with_old_holiday_list_2():
-    # Reference date is before the new holiday calendar
-    df = dip.get_di_data(reference_date="22-12-2023")
-    result = df.query("contract_code == 'N27'")["settlement_rate"].values[0]
-    assert result == 9.809
+def test_settlement_rates_with_current_holiday_list():
+    settlement_rates = {
+        "F24": 11.644,
+        "J24": 11.300,
+        "N24": 10.786,
+        "V24": 10.321,
+        "F25": 10.031,
+        "J25": 9.852,
+        "N25": 9.715,
+        "V25": 9.651,
+        "F26": 9.583,
+        "N26": 9.631,
+        "F27": 9.683,
+        "N27": 9.794,
+        "F29": 10.042,
+        "F31": 10.240,
+        "F33": 10.331,
+    }
+
+    df = dip.get_di_data(reference_date="26-12-2023")
+    contract_codes = list(settlement_rates.keys())
+    results = df.query("contract_code in @contract_codes")["settlement_rate"].to_list()
+    assert results == list(settlement_rates.values())
