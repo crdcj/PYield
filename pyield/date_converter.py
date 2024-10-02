4f0c43c08ccafaa548d6726ff2d5b4519d35e03e
Carlos Carvalho <cr.cj@outlook.com>
2024-10-02 07:48:50 -0300

    fix in date converter

--- pyield/date_converter.py
+++ pyield/date_converter.py
@@ -10,11 +10,9 @@ type PandasArrayDateTypes = pd.Series | pd.DatetimeIndex | pd.Index
 type ArrayDateTypes = PandasArrayDateTypes | np.ndarray | list | tuple
 
 
-def validate_year_format(value):
+def validate_year_format(value) -> None:
     """If the input date is a string, it validates the year format."""
-    if not isinstance(value, str):
-        return
-
+    value = str(value)
     value = value.strip().lower()
     if value in {"today", "now"}:
         return
@@ -22,16 +20,6 @@ def validate_year_format(value):
     if re.match(r"^\d{4}-", value):
         error_msg = f"Invalid format: {value}. Day first format is required. Example: '31-05-2024'."  # noqa
         raise ValueError(error_msg)
-
-
-def check_first_value(dates):
-    match dates:
-        case None:
-            raise ValueError("Date cannot be None.")
-        case str():
-            validate_year_format(dates)
-        case list() | tuple() | np.ndarray() | pd.Series():
-            validate_year_format(dates[0])
 
 
 @overload
@@ -43,25 +31,33 @@ def convert_input_dates(dates: ArrayDateTypes) -> pd.Series: ...
 def convert_input_dates(
     dates: ScalarDateTypes | ArrayDateTypes,
 ) -> pd.Timestamp | pd.Series:
-    check_first_value(dates)
-
-    result = pd.to_datetime(dates, dayfirst=True)
-
-    match result:
-        case pd.Timestamp():
-            result = result.normalize()
-        case dt.datetime() | dt.date() | np.datetime64():
-            result = pd.Timestamp(result).normalize()
-        case pd.Series():
-            result = result.astype("datetime64[ns]")
-        case pd.DatetimeIndex():
-            result = pd.Series(result).astype("datetime64[ns]")
+    match dates:
         case None:
-            raise ValueError("Date cannot be None.")
-        case _:
-            raise ValueError("Invalid date input type.")
+            raise ValueError("'dates' cannot be None.")
 
-    return result
+        case str():
+            validate_year_format(dates)
+            return pd.to_datetime(dates, dayfirst=True)
+
+        case dt.datetime() | dt.date() | np.datetime64() | pd.Timestamp():
+            return pd.Timestamp(dates).normalize()
+
+        case pd.Series() | pd.Index() | np.ndarray() | list() | tuple():
+            result = pd.Series(dates)
+
+            if result.empty:
+                raise ValueError("'dates' cannot be an empty Array.")
+
+            if pd.api.types.is_string_dtype(result):
+                validate_year_format(result[0])
+
+            return pd.to_datetime(result, dayfirst=True).astype("datetime64[ns]")
+
+        case pd.DatetimeIndex():
+            return pd.Series(result).astype("datetime64[ns]")
+
+        case _:
+            raise ValueError("Invalid input type for 'dates'.")
 
 
 def convert_to_numpy_date(
