842271e24e264678ced1de72e5ccdf96ffd14d1e
Carlos Carvalho <cr.cj@outlook.com>
2025-02-07 20:23:00 -0300

    main auction function passed to api root

--- pyield/__init__.py
+++ pyield/__init__.py
@@ -2,6 +2,7 @@ from pyield import anbima, bday
 from pyield.__about__ import __version__
 from pyield.b3_futures import futures
 from pyield.b3_futures.di import DIFutures
+from pyield.bc.auction import auctions
 from pyield.ima import ima
 from pyield.indicators import indicator
 from pyield.interpolator import Interpolator
@@ -12,6 +13,7 @@ from pyield.tpf import lft, ltn, ntnb, ntnf
 __all__ = [
     "__version__",
     "anbima",
+    "auctions",
     "bday",
     "DIFutures",
     "forward_rates",
