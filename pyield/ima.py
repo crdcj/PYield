10df7b0e0128087074cad63dc7ddaac6adcf2beb
Carlos Carvalho <cr.cj@outlook.com>
2025-02-09 17:27:32 -0300

    ima update

--- pyield/ima.py
+++ pyield/ima.py
@@ -1,130 +1,140 @@
 import io
 import logging
-import re
-import warnings
+from typing import Literal
 
 import pandas as pd
 import requests
-from bs4 import XMLParsedAsHTMLWarning
 
-from pyield import date_converter as dc
-from pyield.date_converter import DateScalar
+ima_types = Literal[
+    "IRF-M 1",
+    "IRF-M 1+",
+    "IRF-M",
+    "IMA-B 5",
+    "IMA-B 5+",
+    "IMA-B",
+    "IMA-S",
+    "IMA-GERAL-EX-C",
+    "IMA-GERAL",
+]
 
-IMA_URL = "https://www.anbima.com.br/informacoes/ima/ima-quantidade-mercado.asp"
-COLUMN_MAPPING = {
-    "Data Referência": "Date",
-    "Título": "BondType",
+LAST_IMA_COL_MAPPING = {
+    # "2",
+    "Data de Referência": "Date",
+    "INDICE": "IMAType",
+    "Títulos": "BondType",
     "Data de Vencimento": "Maturity",
-    "Codigo Selic": "SelicCode",
+    "Código SELIC": "SelicCode",
     "Código ISIN": "ISIN",
+    "Taxa Indicativa (% a.a.)": "IndicativeRate",
     "PU (R$)": "Price",
-    "Quantidade em Mercado (1.000 Títulos)": "MarketQuantity",
-    "Valor de Mercado (R$ Mil)": "MarketValue",
-    "Variação da Quantidade (1.000 Títulos)": "QuantityVariation",
-    "Status do Titulo": "BondStatus",
+    # "PU de Juros (R$)": "InterestPrice",
+    "Quantidade (1.000 títulos)": "MarketQuantity",
+    "Quantidade Teórica (1.000 títulos)": "TheoreticalQuantity",
+    "Carteira a Mercado (R$ mil)": "MarketValue",
+    "Peso (%)": "Weight",
+    "Prazo (d.u.)": "BDToMat",
+    "Duration (d.u.)": "Duration",
+    # "Número de Operações *": "NumberOfOperations",
+    # "Quant. Negociada (1.000 títulos) *": "NegotiatedQuantity",
+    # "Valor Negociado (R$ mil) *": "NegotiatedValue",
+    "PMR": "PMR",  # Prazmo médio de repactuação
+    "Convexidade": "Convexity",
 }
 
 
-def _fetch_url_tables(target_date: pd.Timestamp) -> pd.DataFrame:
-    # Warning supression for BeautifulSoup, as the text is valid HTML and not XML
-    warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
+LAST_IMA_URL = "https://www.anbima.com.br/informacoes/ima/arqs/ima_completo.txt"
 
-    target_date_str = target_date.strftime("%d/%m/%Y")
-    payload = {
-        "Tipo": "",
-        "DataRef": "",
-        "Pai": "ima",
-        "Dt_Ref_Ver": "20250117",
-        "Dt_Ref": f"{target_date_str}",
-    }
 
-    r = requests.post(IMA_URL, data=payload, timeout=10)
+def _fetch_last_ima() -> pd.DataFrame:
+    r = requests.get(LAST_IMA_URL)
     r.raise_for_status()
-    r.encoding = "iso-8859-1"
-    if "Não há dados disponíveis" in r.text:
-        logging.warning("No data found. Returning empty DataFrame.")
-        return pd.DataFrame()
+    r.encoding = "latin1"
+    text = r.text.split("2@COMPOSIÇÃO DE CARTEIRA")[1].strip()
+    string_io_buffer = io.StringIO(text)
 
-    string_io_buffer = io.StringIO(r.text)
-
-    dfs = pd.read_html(
+    df = pd.read_csv(
         string_io_buffer,
-        flavor="html5lib",
-        attrs={"width": "100%"},
-        header=0,
-        thousands=".",
+        sep="@",
         decimal=",",
-        displayed_only=True,
+        thousands=".",
         dtype_backend="numpy_nullable",
     )
 
-    df = (
-        pd.concat(dfs)
-        .query("`Data de Vencimento`.notnull()")
-        .query("Título!='Título'")
-        .drop_duplicates(subset="Código ISIN")
-        .reset_index(drop=True)
-    )
-
-    # Convert to CSV and then back to pandas to get automatic type conversion
-    csv_buffer = io.StringIO(df.to_csv(index=False))
-    df = pd.read_csv(csv_buffer, dtype_backend="numpy_nullable")
-
-    # Expressão regular para datas no formato dd/mm/ano
-    date_pattern = r"\b(\d{2}/\d{2}/\d{4})\b"
-
-    # Busca pela primeira data
-    match = re.search(date_pattern, r.text)
-
-    if match:
-        anbima_date_str = match.group(1)
-        anbima_date = pd.to_datetime(anbima_date_str, format="%d/%m/%Y")
-    else:
-        logging.warning("No date found. Returning empty DataFrame.")
-        return pd.DataFrame()
-
-    df["Data Referência"] = anbima_date
-
     return df
 
 
-def _process_df(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.rename(columns=COLUMN_MAPPING)[COLUMN_MAPPING.values()]
+def _process_last_ima(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.rename(columns=LAST_IMA_COL_MAPPING)[LAST_IMA_COL_MAPPING.values()]
+    df["IndicativeRate"] = (df["IndicativeRate"] / 100).round(6)
+    df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%Y")
     df["Maturity"] = pd.to_datetime(df["Maturity"], format="%d/%m/%Y")
-    # Converter a valores para inteiros
     df["MarketQuantity"] = (1000 * df["MarketQuantity"]).astype("Int64")
-    df["MarketValue"] = (1000 * df["MarketValue"]).astype("Int64")
-    df["QuantityVariation"] = (1000 * df["QuantityVariation"]).astype("Int64")
-
+    df["Price"] = df["Price"].round(6)
+    # Duration is in business days, convert to years
+    df["Duration"] /= 252
+    mduration = df["Duration"] / (1 + df["IndicativeRate"])
+    dv01 = 0.0001 * df["Price"] * mduration * df["MarketQuantity"]
+    # Since DV01 and MarketValue are total stock values, we round them to integers
+    df["MarketDV01"] = dv01.round(0).astype("Int64")
+    df["MarketValue"] = (1000 * df["MarketValue"]).round(0).astype("Int64")
     return df
 
 
-def ima(date: DateScalar) -> pd.DataFrame:
-    """
-    Fetch and process IMA market data for a given date.
+def _reorder_last_ima(df: pd.DataFrame) -> pd.DataFrame:
+    col_order = [
+        "Date",
+        "IMAType",
+        "BondType",
+        "Maturity",
+        "SelicCode",
+        "ISIN",
+        "Weight",
+        "IndicativeRate",
+        "Price",
+        "BDToMat",
+        "Duration",
+        "PMR",
+        "Convexity",
+        "TheoreticalQuantity",
+        "MarketDV01",
+        "MarketQuantity",
+        "MarketValue",
+    ]
+    return df[col_order].reset_index(drop=True)
 
-    This function retrieves IMA market data for the specified date from ANBIMA,
-    processes the data into a structured DataFrame, and returns the resulting DataFrame.
+
+def ima(ima_type: ima_types | None = None) -> pd.DataFrame:
+    """
+    Fetch and process the last IMA market data available from ANBIMA.
+
+    This function processes the data into a structured DataFrame.
     It handles conversion of date formats, renames columns to English, and converts
     certain numeric columns to integer types. In the event of an error during data
     fetching or processing, an empty DataFrame is returned.
 
     Args:
-        date (DateScalar): A date-like object representing the target date for fetching
-            the data.
+        ima_type (str, optional): Type of IMA index to filter the data. If None, all
+            IMA indexes are returned. Defaults to None.
 
     Returns:
         pd.DataFrame: A DataFrame containing the IMA data with the following columns:
             - Date: Reference date of the data.
+            - Index: IMA index.
             - BondType: Type of bond.
             - Maturity: Bond maturity date.
             - SelicCode: Code representing the SELIC rate.
             - ISIN: International Securities Identification Number.
             - Price: Bond price.
-            - MarketQuantity: Market quantity (in units of 1000 bonds).
-            - MarketValue: Market value (in units of 1000 reais).
-            - QuantityVariation: Variation in quantity (in units of 1000 bonds).
-            - BondStatus: Status of the bond.
+            - Weight: Weight of the bond in the index.
+            - BDToMat: Business days to maturity.
+            - Duration: Duration of the bond in years.
+            - PMR: Average repurchase term.
+            - Convexity: Convexity of the bond.
+            - TheoreticalQuantity: Theoretical quantity.
+            - MarketDV01: Market DV01 in R$.
+            - MarketQuantity: Market quantity.
+            - MarketValue: Market value in R$.
+
 
     Raises:
         Exception: Logs error and returns an empty DataFrame if any error occurs during
@@ -132,12 +142,13 @@ def ima(date: DateScalar) -> pd.DataFrame:
 
     """
     try:
-        date = dc.convert_input_dates(date)
-        df = _fetch_url_tables(date)
-        df = _process_df(df)
+        df = _fetch_last_ima()
+        df = _process_last_ima(df)
+        df = _reorder_last_ima(df)
+        if ima_type is not None:
+            df = df.query("IMAType == @ima_type").reset_index(drop=True)
+        df = df.sort_values(["IMAType", "BondType", "Maturity"]).reset_index(drop=True)
         return df
     except Exception as e:
-        date_str = date.strftime("%d/%m/%Y")
-        logging.error(f"Error fetching IMA for {date_str}. Returning empty DataFrame.")
-        logging.error(f"Error message: {e}", exc_info=True)
+        logging.exception(f"Error fetching or processing the last IMA data: {e}")
         return pd.DataFrame()
