3e01e6596aa0993d44bcb7ffa420a8e7be93f965
Carlos Carvalho <cr.cj@outlook.com>
2025-02-24 05:48:01 -0300

    ptax module

--- pyield/bc/ptax.py
+++ pyield/bc/ptax.py
@@ -1,23 +1,15 @@
-"""
-Documentação da API do BC
-    https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/documentacao
-Exemplo de chamada:
-    https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='02-19-2025'&@dataFinalCotacao='02-23-2025'&$format=text/csv
-    https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='01-01-2025'&@dataFinalCotacao='2026-01-01'&$format=text/csv"
-
-Primeira data disponível: 28.11.1984
-Última data disponível: data atual
-
-"""
-
+import datetime as dt
 import io
 import logging
+from zoneinfo import ZoneInfo
 
 import pandas as pd
 import requests
 
 from pyield import date_converter as dc
 from pyield.date_converter import DateScalar
+
+TIMEZONE_BZ = ZoneInfo("America/Sao_Paulo")
 
 logger = logging.getLogger(__name__)
 
@@ -30,7 +22,7 @@ COLUMN_MAPPING = {
     "cotacaoVenda": "SellRate",
 }
 
-BASE_API_URL = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?"
+PTAX_API_URL = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?"
 
 
 def _load_from_url(url: str) -> pd.DataFrame:
@@ -54,10 +46,12 @@ def _rename_columns(df: pd.DataFrame) -> pd.DataFrame:
 def _process_df(df: pd.DataFrame) -> pd.DataFrame:
     df["Date"] = df["DateTime"].dt.normalize()
     df["Time"] = df["DateTime"].dt.time
-    df["BuyRate"] = df["BuyRate"].round(4)
-    df["SellRate"] = df["SellRate"].round(4)
+    df["BuyRate"] = df["BuyRate"].round(4)  # BC API retorna com 4 casas decimais
+    df["SellRate"] = df["SellRate"].round(4)  # BC API retorna com 4 casas decimais
+
     df["MidRate"] = (df["BuyRate"] + df["SellRate"]) / 2
     df["MidRate"] = df["MidRate"].round(5)
+
     return df
 
 
@@ -76,6 +70,9 @@ def _fetch_df_from_url(url: str) -> pd.DataFrame:
         df = _process_df(df)
         df = _reorder_and_sort_columns(df)
         return df
+    except requests.exceptions.HTTPError as http_err:
+        logger.error(f"HTTP error occurred: {http_err}")
+        return pd.DataFrame()
     except Exception:
         logger.exception("Error fetching auction data from BC API.")
         return pd.DataFrame()
@@ -85,44 +82,36 @@ def ptax(
     start: DateScalar | None = None,
     end: DateScalar | None = None,
 ) -> pd.DataFrame:
-    """
-    Disponível desde 28.11.1984, refere-se às taxas administradas até março de 1990 e às
-    taxas livres a partir de então (Resolução 1690, de 18.3.1990). As taxas administradas
-    são aquelas fixadas pelo Banco Central; a partir de março de 1992, essa taxa recebeu a
-    denominação de taxa PTAX (fechamento). Até 30 de junho de 2011, as taxas livres
-    correspondiam à média das taxas efetivas de operações no mercado interbancário,
-    ponderada pelo volume de transações do dia. A partir de 1 de julho de 2011 (Circular
-    3506, de 23.9.2010), a Ptax passou a corresponder à média aritmética das taxas obtidas
-    em quatro consultas diárias aos dealers de câmbio e refletem a taxa negociada no
-    momento de abertura da janela de consulta; o boletim de fechamento PTAX corresponde à
-    média aritmética das taxas dos boletins do dia.
+    """Cotações de Dólar PTAX (taxa de câmbio)
+    - Fonte: Banco Central do Brasil (BCB)
+    - Frequência: Diária
+    - Unidade: R$
+
+    Documentação da API do BCB:
+        https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/documentacao
+    Exemplo de chamada à API:
+        https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?
+        @dataInicial='08-01-2025'&@dataFinalCotacao='08-05-2025'&$format=text/csv
 
     **Consultas de Período:**
     - Para consultar dados de um intervalo, forneça as datas de `start` e `end`.
-      Exemplo: `auctions(start='2024-10-20', end='2024-10-27')`
-    - Se apenas `start` for fornecido, a API do BC retornará dados de leilão a partir
-      da data de `start` **até a data mais recente disponível**.
-      Exemplo: `auctions(start='2024-10-20')`
-    - Se apenas `end` for fornecido, a API do BC retornará dados de leilão **desde a
-      data mais antiga disponível até a data de `end`**.
-      Exemplo: `auctions(end='2024-10-27')`
+    Exemplo: `ptax(start='2024-10-20', end='2024-10-27')`
+    - Se apenas `start` for fornecido, a API do BC retornará dados a partir
+    da data de `start` **até a data mais recente disponível**.
+    Exemplo: `ptax(start='2024-10-20')`
+    - Se apenas `end` for fornecido, a API do BC retornará dados **desde a
+    data mais antiga disponível até a data de `end`**.
+    Exemplo: `ptax(end='2024-10-27')`
 
     **Série Histórica Completa:**
-    - Para recuperar a série histórica completa de leilões (desde 12/11/2012 até o
-      último dia útil), chame a função sem fornecer os parâmetros `start` e `end`.
-      Exemplo: `auctions()`
+    - Para recuperar a série histórica completa de leilões (desde 28.11.1984
+    até o último dia útil), chame a função sem fornecer os parâmetros `start` e `end`.
+    Exemplo: `ptax()`
 
-    Busca dados de leilões da API do BC para as datas de início e fim especificadas,
-    filtrando os resultados diretamente na API pelo tipo de leilão, se especificado.
-    O comportamento da função em relação aos parâmetros `start` e `end` segue o padrão
-    da API do Banco Central:
+    Busca dados de cotações de dólar PTAX (taxa de câmbio) para o período:
     - Se `start` for fornecido e `end` não, a função retorna dados de `start` até o fim.
     - Se `end` for fornecido e `start` não, a API retorna dados do início até `end`.
     - Se ambos `start` e `end` forem omitidos, a API retorna a série histórica completa.
-
-    Os dados podem ser filtrados pelo tipo de leilão especificado ("Sell" ou "Buy").
-    Leilões de "Sell" são aqueles em que o Tesouro Nacional vende títulos ao mercado.
-    Leilões de "Buy" são aqueles em que o Tesouro Nacional compra títulos do mercado.
 
     Args:
         start (DateScalar, opcional): A data de início para a consulta dos leilões.
@@ -137,11 +126,23 @@ def ptax(
             Padrão é `None`.
 
     Returns:
-        pd.DataFrame: Um DataFrame contendo dados de leilões para o período e tipo
-            especificados. Em caso de erro ao buscar os dados, um DataFrame vazio
-            é retornado e uma mensagem de erro é registrada no log.
+        pd.DataFrame: Um DataFrame contendo os dados de cotações de dólar PTAX.
+        Se não houver dados disponíveis para o período especificado, um DataFrame vazio
+        será retornado.
 
     Notes:
+        Disponível desde 28.11.1984, refere-se às taxas administradas até março de 1990
+        e às taxas livres a partir de então (Resolução 1690, de 18.3.1990). As taxas
+        administradas são aquelas fixadas pelo Banco Central; a partir de março de 1992,
+        essa taxa recebeu a denominação de taxa PTAX (fechamento). Até 30 de junho de
+        2011, as taxas livres correspondiam à média das taxas efetivas de operações no
+        mercado interbancário, ponderada pelo volume de transações do dia. A partir de
+        1 de julho de 2011 (Circular 3506, de 23.9.2010), a Ptax passou a corresponder
+        à média aritmética das taxas obtidas em quatro consultas diárias aos dealers de
+        câmbio e refletem a taxa negociada no momento de abertura da janela de consulta;
+        o boletim de fechamento PTAX corresponde à média aritmética das taxas dos
+        boletins do dia.
+
         O DataFrame possui as seguintes colunas:
             - Date: Data da cotação.
             - Time: Hora da cotação.
@@ -149,9 +150,9 @@ def ptax(
             - BuyRate: Taxa de compra.
             - SellRate: Taxa de venda.
             - MidRate: Taxa média entre a taxa de compra e venda.
-
+        - Primeira data disponível: 28.11.1984
+        - Última data disponível: data atual
     """
-
     if start:
         start = dc.convert_input_dates(start)
     else:
@@ -160,13 +161,13 @@ def ptax(
     if end:
         end = dc.convert_input_dates(end)
     else:
-        end = pd.Timestamp.now().normalize()
+        end = dt.datetime.now(TIMEZONE_BZ).date()
 
     start_str = start.strftime("%m-%d-%Y")
     end_str = end.strftime("%m-%d-%Y")
 
     # Monta a URL da API com as datas de início e fim
-    url = BASE_API_URL
+    url = PTAX_API_URL
     url += f"@dataInicial='{start_str}'"
     url += f"&@dataFinalCotacao='{end_str}'"
     url += "&$format=text/csv"  # Adiciona o formato CSV ao final
