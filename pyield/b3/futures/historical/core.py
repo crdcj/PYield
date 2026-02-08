8286050a041cb0bcf7e605097b4ee7cbae444224
Carlos Carvalho <carlos.carvalho.jr@gmail.com>
2026-02-08 18:13:12 -0300

    refactor: Rename LAST_SUPPORTED_DATE_OLD_API to DATA_LIMITE_API_ANTIGA for consistency and clarity in core.py

--- pyield/b3/futures/historical/core.py
+++ pyield/b3/futures/historical/core.py
@@ -5,13 +5,12 @@ import polars as pl
 from . import historical_b3 as hb3
 from . import historical_bmf as hmf
 
-LAST_SUPPORTED_DATE_OLD_API = dt.date(2025, 12, 12)
+DATA_LIMITE_API_ANTIGA = dt.date(2025, 12, 12)
 
 
 def _buscar_df_historico(data: dt.date, codigo_contrato: str) -> pl.DataFrame:
     """Busca o histórico do contrato futuro para a data de referência."""
-    if data > LAST_SUPPORTED_DATE_OLD_API:
+    if data > DATA_LIMITE_API_ANTIGA:
         return hb3._buscar_df_historico_b3(data, codigo_contrato)
-    else:
-        # Tenta buscar do serviço histórico antigo
-        return hmf._buscar_df_historico(data, codigo_contrato)
+    # Tenta buscar do serviço histórico antigo
+    return hmf._buscar_df_historico(data, codigo_contrato)
