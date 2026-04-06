d7dccbb1ab6882fc2c3af878e0f7edc155f907c5
Carlos Carvalho <carlos.carvalho.jr@gmail.com>
2026-04-06 14:56:19 -0300

    refactor: remove obsolete ETTJ and IMA modules; update __all__ exports for clarity

--- pyield/anbima/__init__.py
+++ pyield/anbima/__init__.py
@@ -1,15 +1,9 @@
-from pyield.anbima.ettj_intradia import ettj_intradia
-from pyield.anbima.ettj_ultima import ettj_ultima
-from pyield.anbima.ima_ultimo import ima_ultimo
 from pyield.anbima.imaq import imaq
 from pyield.anbima.tpf import tpf, tpf_fonte, tpf_vencimentos
 
 __all__ = [
-    "ima_ultimo",
     "imaq",
     "tpf",
     "tpf_vencimentos",
     "tpf_fonte",
-    "ettj_ultima",
-    "ettj_intradia",
 ]
